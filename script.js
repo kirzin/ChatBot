aca46f9384e681b14cdaadab1df9d36d57f254e9
0xKirz <kirzin06@gmail.com>
2023-01-16 10:24:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = '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';
+const endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  const prompt = document.getElementById('prompt').value;
+
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({ prompt: prompt })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const response = data.choices[0].text;
+    document.getElementById('response').innerHTML = response;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
